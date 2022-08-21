fcb34ab9296ac419979a886e0e6e36fff6a19581
Jan Christoph Bernack <jc.bernack@gmail.com>
2022-08-21 17:16:43 +0200

    parallelize outer loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use rayon::prelude::*;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::time::Instant;
+
+/// Results:
+/// completed in 187.500069011s first attempt
+/// completed in 99.903330992s all loops parallel
+/// completed in 23.887663784s outermost loop in parallel
+/// completed in 15.566494685s remove redundant set copying in the non-parallelized loops
 
 const WORD_LENGTH: u32 = 5;
 // const WORD_COUNT: u32 = 5;
@@ -38,37 +45,39 @@ fn main() {
             println!("number of encoded words {}", encoded_words.len());
             println!("words cooked in {:?}", start.elapsed());
             // find all sets of size N that have no common bits
-            let mut set: Vec<u32> = Vec::new();
-            find_set(&encoded_words, 0, 0, &mut set);
+            find_set_par(&encoded_words);
             println!("completed in {:?}", start.elapsed());
         }
     }
 }
 
-fn find_set(items: &Vec<u32>, offset: usize, bits: u32, set: &mut Vec<u32>) {
-    // TODO: parallelize loop, maybe just the outer loop?
-    for i in offset..items.len() {
-        let item = items[i];
-        if set.len() == 0 && i % 100 == 0 {
+fn find_set_par(items: &Vec<u32>) {
+    (0..items.len()).into_par_iter().for_each(|i| {
+        if i % 100 == 0 {
             println!("{}/{}", i, items.len());
         }
+        find_set(items, i, items[i], &mut vec![items[i]])
+    })
+}
+
+fn find_set(items: &Vec<u32>, offset: usize, bits: u32, set: &mut Vec<u32>) {
+    (offset..items.len()).for_each(|i| {
+        let item = items[i];
         if item & bits == 0 {
-            set.push(item.clone());
+            set.push(item);
             if set.len() == 5 {
                 // TODO: collect and return all sets, as iterator?
                 // TODO: match items in the set to original words and print nicely, also list anagrams
                 println!(
                     "found a set {:?}",
-                    set.iter()
-                        .map(format_encoded_word)
-                        .collect::<String>()
+                    set.iter().map(format_encoded_word).collect::<String>()
                 );
             } else {
                 find_set(items, i + 1, item | bits, set);
             }
             set.pop();
         }
-    }
+    })
 }
 
 // example output: "----e---i-----o-------w--z"
