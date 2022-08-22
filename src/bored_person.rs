6850be1358521c1466cc1b5cda211d3d7c0aedec
Jan Christoph Bernack <jc.bernack@gmail.com>
2022-08-22 16:15:08 +0200

    add more explanations and optimization

--- src/bored_person.rs
+++ src/bored_person.rs
@@ -13,11 +13,20 @@ impl Bitword {
         Self(1 << ((c as u32) - ('a' as u32)))
     }
 
+    fn encode(word: &str) -> Self {
+        word.chars()
+            .fold(Bitword::default(), |bits, c| bits | Bitword::new(c))
+    }
+
     fn empty(&self) -> bool {
         self.0 == 0
     }
 
-    fn len(&self) -> u32 {
+    fn contains(&self, c: char) -> bool {
+        !(*self & Bitword::new(c)).empty()
+    }
+
+    fn letters(&self) -> u32 {
         self.0.count_ones()
     }
 
@@ -90,51 +99,53 @@ pub fn main() {
         .lines()
         .chain(alphabetical.lines())
         .map(|l| {
-            let w = l
-                .chars()
-                .fold(Bitword::default(), |a, c| a | Bitword::new(c));
+            let w = Bitword::encode(l);
             (w, (w, l))
         })
-        .filter(|(w, _)| w.len() == 5)
+        .filter(|(w, _)| w.letters() == 5)
         .unzip();
     words.par_sort_unstable();
     words.dedup();
 
     let words_set = words.iter().collect::<FnvHashSet<_>>();
 
+    println!("{:?} cooking words", start.elapsed());
+
     // build a lookup table mapping from a letter to all words containing that letter
-    let buckets = ('a'..='z')
+    let words_per_letter = ('a'..='z')
+        .par_bridge()
         .map(|c| {
             (
                 c,
-                words
-                    .iter()
-                    .filter(|w| !(**w & Bitword::new(c)).empty())
-                    .collect::<Vec<_>>(),
+                words.iter().filter(|w| w.contains(c)).collect::<Vec<_>>(),
             )
         })
         .collect::<FnvHashMap<_, _>>();
 
     // sort the buckets by the number of words per letter, meaning the first elements will be "rare" letters
-    let mut freq = buckets
+    let mut word_count_per_letter = words_per_letter
         .iter()
         .map(|(c, words)| (*c, words.len()))
         .collect::<Vec<_>>();
-    freq.sort_unstable_by_key(|(_, l)| *l);
+    word_count_per_letter.sort_unstable_by_key(|(_, l)| *l);
 
-    println!("freq: {:?}", freq);
+    let rare_letter1 = word_count_per_letter[0].0;
+    let rare_letter2 = word_count_per_letter[1].0;
+
+    println!("freq: {:?}", word_count_per_letter);
 
     // build a list of all the words that contain at least one of the two least common letters
-    let mut starting_words: Vec<&Bitword> = buckets[&freq[0].0]
+    let mut rare_words: Vec<&Bitword> = words_per_letter[&rare_letter1]
         .iter()
-        .chain(&buckets[&freq[1].0])
+        .chain(&words_per_letter[&rare_letter2])
         .map(|x| *x)
         .collect();
-    starting_words.sort();
-    starting_words.dedup();
+    rare_words.sort();
+    rare_words.dedup();
 
     // println!("starting_words: {:?}", starting_words);
-    println!("number of starting_words: {:?}", starting_words.len());
+    println!("number of rare words: {:?}", rare_words.len());
+    println!("{:?} frequency bucketing", start.elapsed());
 
     // build unique pairs of words with 10 unique letters
     let mut pairs: Vec<Bitword> = words
@@ -146,7 +157,7 @@ pub fn main() {
                 .iter()
                 .skip(i)
                 .map(|w| *w | *word)
-                .filter(|w| w.len() == 10)
+                .filter(|w| w.letters() == 10)
         })
         .collect();
     pairs.par_sort_unstable();
@@ -154,6 +165,7 @@ pub fn main() {
 
     let pairs_set = pairs.iter().collect::<FnvHashSet<_>>();
 
+    println!("{:?} finding pairs", start.elapsed());
     println!("{} pairs found", pairs.len());
 
     // finds the first word w which is contained in the pair p and where the other 5 letters,
@@ -165,38 +177,47 @@ pub fn main() {
                 // overlap 2 letters
                 // p: 000001111111111
                 // w: 111001100000000
-                // ^: 111000011111111 => len 8
+                // ^: 111000011111111 => len 8 => not a word
                 // overlap 5 letters
                 // p: 000001111111111
                 // w: 000001111100000
-                // ^: 000000000011111 => len 5
+                // ^: 000000000011111 => len 5 => valid other word in the pair
                 let x = p ^ *w;
-                (x.len() == 5 && words_set.contains(&x)).then(|| (word_to_str[w], word_to_str[&x]))
+                (x.letters() == 5 && words_set.contains(&x))
+                    .then(|| (word_to_str[w], word_to_str[&x]))
             })
             .unwrap()
     };
 
+    // match pairs to a word containing at least one of the two rarest letters
+    // then determine the remaining 10 letters and check if a pair using those exists
     let solutions = pairs
         .par_iter()
-        .filter(|pair| {
-            // println!("{:?}", **w);
-            (**pair & Bitword::new(freq[0].0)).empty() && (**pair & Bitword::new(freq[1].0)).empty()
-        })
+        // .filter(|pair| {
+        //     // println!("{:?}", **w);
+        //     !pair.contains(rare_letter1) || !pair.contains(rare_letter2)
+        // })
         .flat_map_iter(|pair| {
-            starting_words
+            rare_words
                 .iter()
                 .map(|&sw| (sw, *sw | *pair))
-                .filter(|(_, combined)| combined.len() == 15)
+                .filter(|(_, combined)| combined.letters() == 15)
                 .flat_map(|(sw, combined)| {
-                    let end = !combined;
-                    end.chars()
-                        .map(move |c| end ^ Bitword::new(c))
+                    // end will have all bits set of the 11 remaining, yet unused letters
+                    let remaining = !combined;
+                    remaining
+                        .chars()
+                        // individually toggle each remaining bit off, resulting in 10 bit words
+                        .map(move |c| remaining ^ Bitword::new(c))
+                        // and try to find a pair using those 10 letters
                         .filter(|e| pairs_set.contains(&e))
+                        // if there is one we can complete our 15 letter set with the 10 letters of this pair
                         .map(move |e| (*sw, *pair, e))
                 })
                 .collect::<Vec<(Bitword, Bitword, Bitword)>>()
         })
         .map(|(w, p0, p1)| {
+            // map back to strings
             let (w1, w2) = pair_to_str(p0);
             let (w3, w4) = pair_to_str(p1);
             let mut s = [word_to_str[&w], w1, w2, w3, w4];
@@ -205,6 +226,6 @@ pub fn main() {
         })
         .collect::<FnvHashSet<_>>();
 
-    println!("{:?} elapsed", start.elapsed());
     println!("{} solutions {:?}", solutions.len(), solutions);
+    println!("{:?} total time", start.elapsed());
 }
